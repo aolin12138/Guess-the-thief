a75e8f63524c6dcb82788f148be19d487c63ca8b
amcm539 <adammcmann@Adams-MacBook-Pro-M1.fritz.box>
2024-09-05 00:03:59 +1200

    Added getters to the  PreviousScore class

--- src/main/java/nz/ac/auckland/se206/PreviousScore.java
+++ src/main/java/nz/ac/auckland/se206/PreviousScore.java
@@ -1,11 +1,19 @@
 package nz.ac.auckland.se206;
 
 public class PreviousScore {
-  private String timeCompleted;
-  private int roundNumber;
+  private String roundNumber;
+  private String timeUsed;
 
-  public PreviousScore(String timeCompleted, int roundNumber) {
-    this.timeCompleted = timeCompleted;
-    this.roundNumber = 600 - roundNumber; // Calculate time used from time remaining
+  public PreviousScore(String roundNumber, int secondsRemaining) {
+    this.roundNumber = roundNumber;
+    this.timeUsed = Utils.convertSecondsToTimeFormat(secondsRemaining);
+  }
+
+  public String getRoundNumber() {
+    return this.roundNumber;
+  }
+
+  public String getTimeUsed() {
+    return this.timeUsed;
   }
 }
