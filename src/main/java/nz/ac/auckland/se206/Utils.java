78c9b19a28ea68afc3d9c9b816f609975823e314
amcm539 <adammcmann@Adams-MacBook-Pro-M1.fritz.box>
2024-09-04 23:53:28 +1200

    Added a javadoc so the method is easier to understand

--- src/main/java/nz/ac/auckland/se206/Utils.java
+++ src/main/java/nz/ac/auckland/se206/Utils.java
@@ -3,11 +3,18 @@ package nz.ac.auckland.se206;
 /*Utility methods for common tasks. Feel free to add static methods here. */
 public class Utils {
 
+  /**
+   * Converts the given time in seconds to a formatted time string in the format "MM:SS". The input
+   * seconds represent the time remaining, and the method calculates the time used from a total of
+   * 660 seconds (600 seconds gameplay + 60 seconds to guess).
+   *
+   * @param seconds the time remaining in seconds
+   * @return a formatted time string in the format "MM:SS"
+   */
   public static String convertSecondsToTimeFormat(int seconds) {
     seconds =
-        660
-            - seconds; // Calculate time used from time remaining (600 seconds gameplay + 60 seconds
-                       // to guess)
+        660 - seconds; // Calculate time used from time remaining (600 seconds gameplay + 60 seconds
+    // to guess)
     int minutes = seconds / 60;
     int remainingSeconds = seconds - (minutes * 60);
 
