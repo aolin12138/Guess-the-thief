383070056fe36d39d944bba14bc4b85c6fb505f8
amcm539 <adammcmann@Adams-MacBook-Pro-M1.fritz.box>
2024-09-14 00:28:59 +1200

    Created a timer class and outlined the required methods

---
+++ src/main/java/nz/ac/auckland/se206/Timer.java
@@ -0,0 +1,22 @@
+package nz.ac.auckland.se206;
+
+public class Timer {
+
+  private static double timeToCount;
+  private static double guessTime;
+  private static int progress;
+
+  public static void startTimer() {}
+
+  public static void setPlayTime(int seconds) {}
+
+  public static void setGuessTime(int seconds) {}
+
+  public static double getTime() {
+    return 1;
+  }
+
+  public static int getProgress() {
+    return 1;
+  }
+}
