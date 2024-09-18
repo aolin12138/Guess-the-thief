032bf37f3b9a4bcd986f1a0c6eef2c7ca02aca0e
amcm539 <adammcmann@Adams-MacBook-Pro-M1.fritz.box>
2024-09-19 02:59:45 +1200

    Added two methods for transferring time between scenes

--- src/main/java/nz/ac/auckland/se206/controllers/NewspaperController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/NewspaperController.java
@@ -83,4 +83,12 @@ public class NewspaperController {
     timeline.setCycleCount(Timeline.INDEFINITE);
     timeline.play();
   }
+
+  public static void setTimeToCount(double timeFromPreviousScene) {
+    timeToCount = timeFromPreviousScene;
+  }
+
+  public static void passTimeToCrimeScene(double timeToCount) {
+    CrimeSceneController.setTimeToCount(timeToCount);
+  }
 }
